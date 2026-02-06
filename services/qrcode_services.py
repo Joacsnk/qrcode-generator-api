06868affc1b435ff41715358f22e3775c431cb07
Jackson Lobo <Joacsnk@gmail.com>
2026-02-06 11:28:27 -0300

    feat: qrcode more readable

--- services/qrcode_services.py
+++ services/qrcode_services.py
@@ -1,26 +1,40 @@
 import qrcode
 from qrcode.image.svg import SvgImage # Gerador em SVG
 from io import BytesIO # Simulador de arquivo em memória
+from qrcode.constants import ERROR_CORRECT_H # Correção de erro
 
-def qrcode_generator(url: str, img_type: str = "png") -> BytesIO:
+
+def qrcode_generator(url: str, img_type: str = "PNG") -> BytesIO:
     
     if not url: 
         raise ValueError("URL inválida")
+    
+    img_type = img_type.upper()
+    
 
-    qr = qrcode.make(url) # Criação do qrcode
+    qr = qrcode.QRCode(
+            version=None,                 
+            error_correction=ERROR_CORRECT_H, # Melhor legibilidade no qrcode
+            box_size=10,                   # Tamanho dos quadrados
+            border=4                       # Margem
+        )
+
+    qr.add_data(url)
+    qr.make(fit=True)
+    
     buffer = BytesIO() # Criando a memória
 
-    img_type = img_type.lower()
+    if img_type in ("PNG", "JPEG"): # Salvando em PNG ou JPEG
+        img = qr.make_image(
+            fill_color="black",
+            back_color="white"
+        )
 
-    if img_type == "png": # Salvando em PNG
-        qr.save(buffer, format="PNG")
+        img.save(buffer, format=img_type)    
+        
+    elif img_type == "SVG": # Salvando em svg
     
-    elif img_type == "jpeg": # Salvando em JPEG
-        qr.save(buffer, format="JPEG")
-    
-    elif img_type == "svg": # Salvando em svg
-        factory = SvgImage
-        img = qrcode.make(url, image_factory=factory)
+        img = qr.make_image(image_factory=SvgImage)
         img.save(buffer)
         
     else:
